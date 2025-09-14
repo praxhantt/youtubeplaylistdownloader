d886cc2a20622e96df86dc0c40527d19473abf6f
PRASHANT VISHWAKARMA <157842005+praxhantt@users.noreply.github.com>
2025-09-14 12:18:04 +0530

    Add files via upload

---
+++ ytplaylistdownloader.py
@@ -0,0 +1,19 @@
+import yt_dlp
+
+playlist_url = "https://youtu.be/vB0V3iCSzQw?si=ToSOcLMgNLMGXYY8"  # Replace with your playlist URL
+download_path = "D:/Downloads/ytplaylistdo"  # Replace with your download folder
+
+ydl_opts = {
+    'outtmpl': f'{download_path}/%(playlist_index)s - %(title)s.%(ext)s',  # Filename template with playlist index
+    'format': 'bestvideo+bestaudio/best',  # Best video and audio merged
+    'ignoreerrors': True,
+    'quiet': False,
+    'noplaylist': False,  # Download entire playlist
+    'restrictfilenames': True,  # Avoid special characters in filenames
+    'nopart': True,  # Disable .part files for simplicity
+    'overwrites': True,  # Overwrite existing files
+    'merge_output_format': 'mp4',  # Force merged video to be in mp4 format
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([playlist_url])
